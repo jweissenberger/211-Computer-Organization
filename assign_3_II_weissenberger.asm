04008e813c5a48ac128e8769cdb58c005a668ada
John Weissenberger <johnaweissenberger@gmail.com>
2019-02-05 10:29:57 -0500

    start final part of lab

---
+++ assign_3_II_weissenberger.asm
@@ -0,0 +1,10 @@
+# Jack Weissenberger
+# assign_3_II_weissenberger
+
+.data
+	N: .word 7 # this is the length of the list
+	sum: .word 0 # the sum of the list
+	max: .word 0 # max of the list
+	hld1: .word 0xAAAAAAAA
+	hld2: .word 0xFFFFFFFF
+	vals: .word 5, 8, 1, -3, 7, 10, 2
--- assign_3_I_6_weissenberger.asm
+++ assign_3_I_6_weissenberger.asm
@@ -5,3 +5,36 @@
 	a: .word 2 # $s0
 	b: .word 2 # $s1
 	i: .word 0 # $t0
+	j: .word 0 # $t1
+	D: .word 2, 3, 4, 5 # base address in $s2
+
+.text
+.globl main
+
+main:
+	# load in each of the variables
+	lw $s0, a
+	lw $s1, b
+	lw $t0, i
+	lw $t1, j
+	la $s2, D 
+	
+
+Loop1:   
+	slt $t2, $t0, $s0      # i < a
+   	beq $t2, $0, End       # i >= a, go to end
+   	addi $t0, $t0, 1       # i += 1
+
+Loop2:   
+	slt $t2, $t1, $s1      # j < b
+   	beq $t2, $0, Loop1     # j >= b go to loop1
+   	add $t2, $t0, $t1      # i+j
+   	sll $t4, $t1, 4        # $t4 = 4*j   
+   	add $t3, $t4, $s2      # $t3 = &D[4*j]
+   	sw $t2, 0($t3)         # D[4*j] = i+j
+   	addi $t1, $t1, 1       # j += 1
+   	j Loop2			# loop2 again
+
+End:
+	li $v0, 10
+	syscall
