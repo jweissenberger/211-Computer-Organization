7f296251ffb227530947e0d0389522ee19750fb1
John Weissenberger <johnaweissenberger@gmail.com>
2019-02-05 09:19:58 -0500

    assign 2 start

---
+++ assign_3_I_1_weissenberger.asm
@@ -0,0 +1,19 @@
+# Jack Weissenberger 2.20
+# assign_3_I_1_weissenberger.s
+
+.data
+t0val: .word 0x8F0F0F0F
+t1val: .word 0X90A0A0A0
+
+.text
+.globl main
+
+main: 
+	lw $t0, t0val
+	lw $t1, t1val
+	
+	srl $t2, $t0, 10  # shift t0 right by 10
+	sll $t2, $t2, 26  # shift above result 26 to left st 0-25 is zero
+	sll $t1, $t1, 6   # shift left t1 by 6
+	srl $t1, $t1, 6   # shift right t1 by six so last contain zeros
+	add $t1, $t2, $t1 # add t1 and t2, t1 will have the result
---
+++ assign_3_I_6_weissenberger.asm
@@ -0,0 +1,7 @@
+# Jack Weissenberger
+# 2.27 assign_3_I_6_weissenberger
+
+.data
+	a: .word 2 # $s0
+	b: .word 2 # $s1
+	i: .word 0 # $t0
