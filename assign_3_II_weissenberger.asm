90f9b13a48174e83e0a0bd00a001b0e9243d9b08
John Weissenberger <johnaweissenberger@gmail.com>
2019-02-05 14:57:03 -0500

    finish assign 3

--- assign_3_II_weissenberger.asm
+++ assign_3_II_weissenberger.asm
@@ -1,6 +1,10 @@
 # Jack Weissenberger
 # assign_3_II_weissenberger
 
+# the sum will be stored in $t0
+# the max will be stored in $t4
+# in buble the list will update in place and then the whole list will be printed out in the end
+
 .data
 	N: .word 7 # this is the length of the list
 	sum: .word 0 # the sum of the list
@@ -8,7 +12,7 @@
 	hld1: .word 0xAAAAAAAA
 	vals: .word 5, 8, 1, -3, 7, 10, 2
 	sum_message: .asciiz "The sum of the array is: "
-	loop_message: .asciiz "\nThe array is: "
+	loop_message: .asciiz "\nAfter bubble, the array is: "
 	max_message: .asciiz "\nThe max of the arrray is: "
 	hld2: .word 0xFFFFFFFF
 		
@@ -72,6 +76,38 @@ max_end:
 	li $v0, 1
 	move $a0, $t4
 	syscall
+	
+	#**BUBBLE LOOP** 
+	la $t1, vals  # get array address
+	la $t7, vals   # $t7 will hold the next value
+	addi $t7, $t7, 4 # get the address of the next value of the array
+	li $t2, 1  # set loop counter (one higher because were comparing value in front)
+	#lw $t7, max  # set max to be zero
+
+bub_loop:
+	beq $t2, $t3, bub_end # check for array end
+	
+	lw  $a0, ($t1) # put value of the array in 
+	lw  $a1, ($t7) # put the following value of the arrry in 
+	slt $s1, $a1, $a0 # compare current value and max
+	bne $s1, $zero, switch_bub # if the next value is greater than the max, go there
+	
+	addi $t2, $t2, 1 # advance loop counter
+	addi $t1, $t1, 4 # advance first array pointer
+	addi $t7, $t7, 4 # advance second array pointer
+	j bub_loop
+switch_bub:
+	#lw $t4, ($t1) # make the max the currect value of the array
+	lw $t9, ($t1) # store previous in temporary address
+	lw $t8, ($t7) # store the next in a temp address
+	#  swap them
+	sw $t9, 0($t7)
+	sw $t8, 0($t1)
+	addi $t2, $t2, 1 # advance loop counter
+	addi $t1, $t1, 4 # advance array pointer
+	addi $t7, $t7, 4 # advance second array pointer
+	j bub_loop
+bub_end:
 	
 	
 	#**PRINT LOOP**
