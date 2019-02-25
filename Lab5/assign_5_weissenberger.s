0a15698f5fe903420b073d1df69e2fd87cf0c691
John Weissenberger <johnaweissenberger@gmail.com>
2019-02-25 09:15:20 -0500

    Assignment 6

--- Lab5/assign_5_weissenberger.s
+++ Lab5/assign_5_weissenberger.s
@@ -1,3 +1,6 @@
+# Jack Weissenberger
+# 211 assignment 5
+
 .data
 	max:    .word   0
 	product:.word	0
@@ -19,55 +22,55 @@ main:
 	la	$a0, str1	# print the prompt
 	syscall
 
-	#Get the first number
+	# first number
 	li	$v0, 6		# put first fp into f0
-	syscall			#Do the read
-	mov.s	$f12, $f0	#Copy $f0 to $f12 for printing
-	mov.s	$f1, $f0	#Copy $f0 to $f1 for calculating
+	syscall			
+	mov.s	$f12, $f0	# $f0 to $f12
+	mov.s	$f1, $f0	# $f0 to $f1
 
 	li	$v0, 2		#Print FP from $f12
-	syscall			#Do the print
+	syscall		
 
-	li	$v0, 4		#Print a string
-	la	$a0, crlf	#Print cr and lf
+	li	$v0, 4		#nl
+	la	$a0, crlf	
 	syscall
 	
 
-	#Get the second number
-	li	$v0, 6		#Read FP into $f0
-	syscall			#Do the read
+	# second number
+	li	$v0, 6		# ead FP into $f0
+	syscall			
 
-	mov.s	$f12, $f0	#Copy $f0 to $f12 for printing
-	mov.s	$f2, $f0	#Copy $f0 to $f2 for calculating
+	mov.s	$f12, $f0	# $f0 to $f12 for printing
+	mov.s	$f2, $f0	# $f0 to $f2
 
-	li	$v0, 2		#Print FP from $f12
-	syscall			#Do the print
+	li	$v0, 2		# fp from $f12
+	syscall			
 
-	li	$v0, 4		#Print a string
-	la	$a0, crlf	#Print cr and lf
+	li	$v0, 4		# newline
+	la	$a0, crlf
 	syscall
 	
 	#Get the third number 
-	li	$v0, 6		#Read FP into $f0
-	syscall			#Do the read
-	mov.s	$f12, $f0	#Copy $f0 to $f12 for printing
-	mov.s	$f3, $f0	#Copy $f0 to $f3 for calculating
+	li	$v0, 6		# read FP into $f0
+	syscall			# read
+	mov.s	$f12, $f0	# copy $f0 to $f12 for printing
+	mov.s	$f3, $f0	# copy $f0 to $f3 for calculating
 
-	li	$v0, 2		#Print FP from $f12
-	syscall			#Do the print
+	li	$v0, 2		# print fp
+	syscall			
 
-	li	$v0, 4		#Print a string
-	la	$a0, crlf	#Print cr and lf
+	li	$v0, 4		# newline
+	la	$a0, crlf	
 	syscall
 	
 	
-	#Print the string for unbiased and biased exponents and significand
-	li	$v0, 4		#Syscall code for print string
-	la	$a0, str2	#Address of string to print
+	# string for unbiased and biased exponents and significand
+	li	$v0, 4	
+	la	$a0, str2	
 	syscall
 	
-	#print biased exponent
-	mfc1 	$t0, $f1	#Move float from coprocessor 1 to register $t1
+	# print biased exponent
+	mfc1 	$t0, $f1	# move float into $t0
 	sll 	$t0, $t0, 1
 	srl 	$t0,$t0, 24
 	li 	$v0, 4
@@ -81,7 +84,7 @@ main:
 	la	$a0, crlf	#Print cr and lf
 	syscall
 	
-	#print unbiased exponent
+	# print unbiased exponent
 	addi 	$t0, $t0, -127
 	li 	$v0, 4
 	la 	$a0, unbiased
@@ -90,15 +93,15 @@ main:
 	add 	$a0, $t0, $zero
 	syscall
 	
-	li	$v0, 4		#Print a string
+	li	$v0, 4		# print string
 	la	$a0, crlf	#Print cr and lf
 	syscall
 	
-	#print significand
-	mfc1 	$t1, $f1	#Move float from coprocessor 1 to register $t1
+	# significand
+	mfc1 	$t1, $f1	# move float from coprocessor 1 to register $t1
 	sll 	$t1, $t1, 9
 	srl	$t1, $t1, 9
-	addi	$t1, $t1, 838608
+	addi	$t1, $t1, 8388608
 	li 	$v0, 4
 	la 	$a0, sig
 	syscall
@@ -106,68 +109,64 @@ main:
 	add	$a0, $t1, $zero
 	syscall
 	
-	li	$v0, 4		#Print a string
-	la	$a0, crlf	#Print cr and lf
+	li	$v0, 4
+	la	$a0, crlf	# newline
 	syscall
 	
-        
-        #Calculate the max, sum, and product
-        li	$v0, 4		#Syscall code for print string
-	la	$a0, str3	#Address of string to print
+        li	$v0, 4
+	la	$a0, str3	# print string
 	syscall
 	
 	#max
-	c.lt.s    $f2, $f1   	#If $f2 < $f1, code = 1 , else: code = 0 
-	bc1t      0, printMax1  #Branch if true
-	c.lt.s    $f1, $f2   	#If $f2 < $f1, code = 1 , else: code = 0 
-	bc1t      0, printMax2  #Branch if true
+	c.lt.s    $f2, $f1   	# if $f2 < $f1, code = 1 , else: code = 0 
+	bc1t      0, printMax1  # go to max1 if true
+	c.lt.s    $f1, $f2   	# if $f2 < $f1, code = 1 , else: code = 0 
+	bc1t      0, printMax2  # go to max2 if true
 	
 
 	
 next:
-    	mov.s $f12, $f6		# Print max
+    	mov.s $f12, $f6		# max
     	li $v0, 2
     	syscall
-	li	$v0, 4		#Print a string
+	li	$v0, 4		#Print string
 	la	$a0, crlf	#Print cr and lf
 	syscall
 	
-	#sum
-        jal	a3fp    	#Calculate the sum
-	s.s	$f4, sum	#Store the sum into memory
+        jal	a3fp    	# calculate the sum
+	s.s	$f4, sum
 
-	mov.s	$f12, $f4	#Get the sum in $f12 for printing
-	li	$v0, 2		#Print sum
+	mov.s	$f12, $f4
+	li	$v0, 2		# print sum
 	syscall
 
-	li	$v0, 4		#Print a string
-	la	$a0, crlf	#Print cr and lf
+	li	$v0, 4
+	la	$a0, crlf	# print the new line
 	syscall
 	
-	#product
-	jal	m3fp		#Calculate the product
-	s.s	$f5, product	#Store the product into memory
+	jal	m3fp
+	s.s	$f5, product	# store the product
 
-	mov.s	$f12, $f5	#Put the product in $f12 for printing
-	li	$v0, 2		#Print product
+	mov.s	$f12, $f5	# print product
+	li	$v0, 2	
 	syscall
 	
         
 
-	li	$v0, 10		#Call to exit
+	li	$v0, 10		# end program
 	syscall
 
 
 
-#Add($f1 + $f2)+$f3 and put the sum in $f4
+# add($f1 + $f2)+$f3 and put the sum in $f4
 a3fp:	add.s	$f8, $f1, $f2
 	add.s	$f4, $f8, $f3
-	jr	$ra		#Return to caller
+	jr	$ra		# return
 
-#Multiply($f1 * $f2) * $f3 and put the product in $f5
+# multiply($f1 * $f2) * $f3 and put the product in $f5
 m3fp:	mul.s	$f7, $f1, $f2
 	mul.s	$f5, $f7, $f3
-	jr	$ra		#Return to caller
+	jr	$ra		# return
 
 printMax1:   
 	
